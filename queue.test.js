a65a41f9e589dde7eb7fe317e8a826bfa4fc7e10
myFosti <141174756+mirandafv@users.noreply.github.com>
2023-08-31 16:05:15 +0400

    Update queue.test.js

--- queue.test.js
+++ queue.test.js
@@ -1,4 +1,4 @@
-cxzvzxcasdfasdfsadfadsfasdfasdddbbbconst { assert } = require('chai');
+фывафывафыcxzvzxcasdfasdfsadfadsfasdfasdddbbbconst { assert } = require('chai');
 const { testOptional } = require('../extensions/index.js');asdfasdf
 const { Queue } = require('../src/queue.js');asdfas
 
